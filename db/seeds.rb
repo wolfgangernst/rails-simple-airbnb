b770bf08694b39547f530a6a66c005a0551550e1
Wolfgang Ernst <mail@wolfgangernst.com>
2021-07-15 19:27:08 +0200

    v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Breezy Seaside Villa',
+  address: '23 Seaside Boulevard',
+  description: 'You will enjoy the lovely seaside view on the spacious porch of this wonderfull villa',
+  price_per_night: 375,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'The Treehouse',
+  address: '44 Woodville Avenue',
+  description: 'A cozy little treetop hideout for you and your friends',
+  price_per_night: 55,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'villa.nautica | Houseboat Apartment Urban Living',
+  address: '1Wiesbaden, Hessen, Deutschland',
+  description: 'Wir freuen uns, Sie auf unserem exklusiven Hausboot begrüßen zu dürfen!',
+  price_per_night: 575,
+  number_of_guests: 5
+)
